3d69867a2934549f22cea88d5bf31fd94d951116
manny01 <manny01@manny01-PC>
2012-11-20 17:51:11 -0500

    COSC 650 Project First Commit

---
+++ Project 1/src/org/ques1/TCPClient.java
@@ -0,0 +1,17 @@
+package org.ques1;
+
+public class TCPClient {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+	
+	
+
+}
---
+++ Project 1/src/org/ques2/UDPServer.java
@@ -0,0 +1,13 @@
+package org.ques2;
+
+public class UDPServer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
